a7e2fd9f77dc4076e9a5e1f71637e9a5ee508ebb
Gabriel Landry <gablandry31@gmail.com>
2024-09-09 11:06:24 -0400

    feat: ajout des draw_commands.cpp

--- src/draw_commands.cpp
+++ src/draw_commands.cpp
@@ -1,34 +1,35 @@
 #include "draw_commands.h"
 
-
-DrawArraysCommand::DrawArraysCommand(VertexArrayObject& vao, GLsizei count)
+DrawArraysCommand::DrawArraysCommand(VertexArrayObject &vao, GLsizei count)
+    : m_vao(vao), m_count(count)
 {
-    // TODO
 }
 
 void DrawArraysCommand::draw()
 {
-    // TODO
+  m_vao.bind();
+  // P.32 du powerpoint 2
+  glDrawArrays(GL_TRIANGLES, 0, m_count);
 }
 
 void DrawArraysCommand::setCount(GLsizei count)
 {
-    // TODO
+  m_count = count;
 }
 
-DrawElementsCommand::DrawElementsCommand(VertexArrayObject& vao, GLsizei count, GLenum type)
+DrawElementsCommand::DrawElementsCommand(VertexArrayObject &vao, GLsizei count, GLenum type)
+    : m_vao(vao), m_count(count), m_type(type)
 {
-    // TODO
 }
 
 void DrawElementsCommand::draw()
 {
-    // TODO
+  m_vao.bind();
+  // P.33 du powerpoint 2
+  glDrawElements(GL_TRIANGLES, m_count, m_type, nullptr);
 }
 
 void DrawElementsCommand::setCount(GLsizei count)
 {
-    // TODO
+  m_count = count;
 }
-
-
