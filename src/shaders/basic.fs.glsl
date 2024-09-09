0a9c68cdf0a3be2f993d35b4ba83db524aa8f28e
Marianna <marianna@Marianna-DESKTOP.>
2024-09-09 12:32:48 -0400

    basic shaders

--- src/shaders/basic.fs.glsl
+++ src/shaders/basic.fs.glsl
@@ -1,3 +1,13 @@
 #version 330 core
 
 // TODO
+
+out vec4 color;
+
+void main() {
+    color = vec4(1.0, 0.0, 0.0, 1.0); // Rouge opaque //couleur pris de chatGPT
+}
+
+//Le fragment shader fait simplement
+//l’assignation d’une variable out de type vec4 pour la couleur de votre choix avec un alpha de 1
+//(attention d’avoir une couleur différente du clearColor).
