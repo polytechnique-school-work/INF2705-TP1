efe0a77f47b93dd1c14698a6a107a2fd249ac6b1
Gabriel Landry <gablandry31@gmail.com>
2024-09-11 15:15:17 -0400

    Mise en place des scenes

--- src/scenes/scene_triangle.cpp
+++ src/scenes/scene_triangle.cpp
@@ -2,14 +2,24 @@
 
 #include "vertices_data.h"
 
-SceneTriangle::SceneTriangle(Resources& res)
-: Scene(res)
+SceneTriangle::SceneTriangle(Resources &res)
+    : Scene(res)
 {
-    // TODO
+  // TODO
+
+  // 1. Initialiser les shaders
+  m_resources.initShaderProgram(m_resources.basic, "shaders/basic.vs.glsl", "shaders/basic.fs.glsl");
+
+  // 2. Utiliser les vertices
+  m_resources.coloredTriangleBuffer.allocate(GL_ARRAY_BUFFER, sizeof(triVertices), triVertices, GL_STATIC_DRAW);
+
+  // 3. Configurer les buffers et les attributs de vertex
+
+  // 4. Dessiner le triangle
+  m_triangleDraw.draw();
 }
 
-void SceneTriangle::run(Window& w)
+void SceneTriangle::run(Window &w)
 {
-    // TODO
+  // TODO
 }
-
